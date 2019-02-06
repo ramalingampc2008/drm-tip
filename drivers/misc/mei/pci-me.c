projectacrn/acrn-kernel

mei: me: add ice lake point device ids
Add icelake device ids: ICP LP, N and H

Change-Id: I6591d64112bec55bbbea2f72bf2b13eb3327ea96
Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

drivers/misc/mei/pci-me.c
@@ -105,6 +105,10 @@ static const struct pci_device_id mei_me_pci_tbl[] = {
 	{MEI_PCI_DEVICE(MEI_DEV_ID_CNP_H_2, MEI_ME_PCH8_CFG)},
 	{MEI_PCI_DEVICE(MEI_DEV_ID_CNP_H_4, MEI_ME_PCH8_CFG)},
 
+	{MEI_PCI_DEVICE(MEI_DEV_ID_ICP_LP, MEI_ME_PCH8_CFG)},
+	{MEI_PCI_DEVICE(MEI_DEV_ID_ICP_N, MEI_ME_PCH8_CFG)},
+	{MEI_PCI_DEVICE(MEI_DEV_ID_ICP_H, MEI_ME_PCH8_CFG)},
+
 	/* required last entry */
 	{0, }
 };

ramalingampc2008/drm-tip

drivers/misc/mei/pci-me.c
@@ -105,6 +105,10 @@ static const struct pci_device_id mei_me_pci_tbl[] = {
 	{MEI_PCI_DEVICE(MEI_DEV_ID_CNP_H, MEI_ME_PCH12_CFG)},
 	{MEI_PCI_DEVICE(MEI_DEV_ID_CNP_H_4, MEI_ME_PCH8_CFG)},
 
+	{MEI_PCI_DEVICE(MEI_DEV_ID_ICP_LP, MEI_ME_PCH12_CFG)},
+	{MEI_PCI_DEVICE(MEI_DEV_ID_ICP_N, MEI_ME_PCH12_CFG)},
+	{MEI_PCI_DEVICE(MEI_DEV_ID_ICP_H, MEI_ME_PCH12_CFG)},
+
 	/* required last entry */
 	{0, }
 };